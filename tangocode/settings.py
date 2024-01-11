56e872f1f0464c1ebada8f804e73d4ff0b22a067
Grace How <gracehow8702@gmail.com>
2024-01-11 17:51:54 +0000

    Add Herokuapp link

--- tangocode/settings.py
+++ tangocode/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-graceghb-djangocode-hf4durkvk0h.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-graceghb-djangocode-hf4durkvk0h.ws-eu107.gitpod.io', 'djangocode-36061f9aed80.herokuapp.com']
 
 
 # Application definition
